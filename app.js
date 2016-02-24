4fefd3a1cc0a7dfd7ad0dd254b83dfe30235b8d4
Malathi <malathim993@gmail.com>
2016-02-24 11:27:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-
 var http = require('http');
 
 // get the port from the environment variable "VCAP_APP_PORT"
-var port = process.env.VCAP_APP_PORT || 1337;
+var port = 8083;
+var host = "0.0.0.0";
 
 //Start Server to written "Hello World" to all requests
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
-}).listen(port);
+    res.end('Hello World\nThis is Sample nOdeJs application\n');
+}).listen(port,host);
 
-console.log('Server running at http://127.0.0.1:'+port);
+console.log('Server running at http://0.0.0.0:'+port);
